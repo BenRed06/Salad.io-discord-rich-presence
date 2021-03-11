7af85729799e9573ec113969042c991257c600fd
BenRed <64615939+BenRed06@users.noreply.github.com>
2021-03-10 20:30:32 -0500

    Update presence.py

--- presence.py
+++ presence.py
@@ -1,1 +1,11 @@
+from pypresence import Presence
+import time
 
+client_id = 'your client ID'  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
+print(RPC.update(state="use code: 5H9AJ2 for 2X rewards! ", details="the gamers crypto miner!"))  # Set the presence
+
+while True:  # The presence will stay on as long as the program is running
+    time.sleep(15) # Can only update rich presence every 15 seconds
