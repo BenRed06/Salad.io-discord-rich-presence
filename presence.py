c4d0865999c7e321160ccdc2e280f97b49f15bff
BenRed <64615939+BenRed06@users.noreply.github.com>
2021-03-10 21:29:30 -0500

    Update presence.py

--- presence.py
+++ presence.py
@@ -8,7 +8,7 @@ RPC.connect()
 #print(RPC.update(state="referal code:", details="currently Chopping"))  # Set the presence
 print(RPC.update(buttons=[{"label": "Get Salad!", "url": "https://www.salad.io"}])) # Can specify up to 2 buttons
 
-RPC.update(details="🔪 Currently Chopping", state="😀 Referal code: 5H9AJ2",  large_image='1024', small_image='discord.png', large_text='SALAD.IO', small_text='text here', start=time.time())
+RPC.update(details="🔪 Currently Chopping", state="😀 Referal code: 5H9AJ2",  large_image='image', small_image='discord.png', large_text='SALAD.IO', small_text='text here', start=time.time())
 
 while 1:
     time.sleep(15) #Can only update presence every 15 seconds
